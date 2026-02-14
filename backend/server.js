1cd1775a3f8e6e5f5e67059c411f6b196ddb32ff
Maxamed459 <maxamedmahdi459@gmail.com>
2026-02-14 12:13:23 +0300

    updated server.js added cookie-parser middleware

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import express from "express"
 import authRouter from "./routes/auth.router.js";
 import { connectDB } from "./config/db.js";
 import { PORT } from "./config/config.js";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
     return res.status(200).json({
